cdc2cb3bb4d40dd20e8469a18a6ea82c96892536
Dimas nugroho <144247337+dimas1625@users.noreply.github.com>
2026-06-26 10:36:37 +0700

    feat: storebook request dan update

---
+++ app/Http/Requests/UpdateBookRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'category_id' => 'required|exists:categories,id',
+            'title' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+            'year' => 'required|integer|min:1800|max:' . date('Y'),
+            'stock' => 'required|integer|min:0',
+            'description' => 'nullable|string',
+        ];
+    }
+}
